edc368b437b5632def426ac1f6a620c6ce96dd81
LOYANNE <loyanne.cristine@gmail.com>
2020-08-09 20:44:25 +0100

    Keep session opened after registering a pressure

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -89,7 +89,6 @@ class RegisterPressureIntentHandler(AbstractRequestHandler):
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                .set_should_end_session(True)
                 # .ask("add a reprompt if you want to keep the session open for the user to respond")
                 .response
         )
