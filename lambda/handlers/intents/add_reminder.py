d12aebbf37777afab39215979b76428859666d11
LOYANNE <loyanne.cristine@gmail.com>
2020-08-12 22:21:48 +0100

    Set absolute reminder

--- lambda/handlers/intents/add_reminder.py
+++ lambda/handlers/intents/add_reminder.py
@@ -12,18 +12,33 @@ class AddReminderIntentHandler(BaseIntentHandler):
 
     def handle(self, handler_input):
         system = self.handler_input.request_envelope.context.system
+        time = self.slot('time')
+        if time == 'EV':
+            time = '18:00'
+        elif time == 'NI':
+            time = '20:00'
+        elif time == 'MO':
+            time = '06:00'
+        elif time == 'AF':
+            time = '14:00'
+        hour = time.split(':')[0]
+        minute = time.split(':')[1]
         json = {
             "requestTime" : datetime.datetime.now().isoformat(),
             "trigger": {
-                "type" : "SCHEDULED_RELATIVE",
-                "offsetInSeconds" : "10"
+                "type" : "SCHEDULED_ABSOLUTE",
+                "recurrence" : {
+                    "recurrenceRules" : [
+                        f"FREQ=DAILY;BYHOUR={hour};BYMINUTE={minute};",
+                    ]
+                }
             },
             "alertInfo": {
                 "spokenInfo": {
                     "content": [{
                         "locale": "en-US",
-                        "text": "walk the dog",
-                        "ssml": "<speak> walk the dog</speak>"
+                        "text": "It's time to register your blood pressure.",
+                        "ssml": "<speak> It's time to register your blood pressure.</speak>"
                     }]
                 }
             },
@@ -41,7 +56,7 @@ class AddReminderIntentHandler(BaseIntentHandler):
 
         self._log(f'Status code: {response.status_code}')
         self._log(response.json())
-        speak_output = 'Ready to go'
+        speak_output = 'Alright, I will remind you when the time comes.'
 
         return (
             handler_input.response_builder
