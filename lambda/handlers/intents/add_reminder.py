c844ab0e1fec263c412e39f361c9297c493d796f
LOYANNE <loyanne.cristine@gmail.com>
2020-08-12 21:14:42 +0100

    Test relative reminder

--- lambda/handlers/intents/add_reminder.py
+++ lambda/handlers/intents/add_reminder.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import requests
+
 from .base_intent_handler import BaseIntentHandler
 from core import Pressure, PressureStatus
 
@@ -8,6 +10,29 @@ class AddReminderIntentHandler(BaseIntentHandler):
         super().__init__('AddReminderIntent')
 
     def handle(self, handler_input):
+        json = {
+            "requestTime" : "2019-09-22T19:04:00.672",
+            "trigger": {
+                "type" : "SCHEDULED_RELATIVE",
+                "offsetInSeconds" : "10"
+            },
+            "alertInfo": {
+                "spokenInfo": {
+                    "content": [{
+                        "locale": "en-US",
+                        "text": "walk the dog",
+                        "ssml": "<speak> walk the dog</speak>"
+                    }]
+                }
+            },
+            "pushNotification" : {
+                "status" : "ENABLED"
+            }
+        }
+
+        response = requests.post('https://httpbin.org/post', json=json)
+        self._log(f'Status code: {response.status_code}')
+        self._log(response.json())
         speak_output = 'Ready to go'
 
         return (
