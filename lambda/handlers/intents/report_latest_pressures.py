380bcccc215c8d0ac67a6b973f24735d54e09780
LOYANNE <loyanne.cristine@gmail.com>
2020-08-12 19:04:51 +0100

    Get images from Google Drive

--- lambda/handlers/intents/report_latest_pressures.py
+++ lambda/handlers/intents/report_latest_pressures.py
@@ -13,16 +13,15 @@ class ReportLatestPressuresIntentHandler(BaseIntentHandler):
     def handle(self, handler_input):
         latest_number = 5
         all_pressures = self.all_pressures()
-        latest_pressures = all_pressures[-min(latest_number, len(all_pressures)):0]
+        latest_pressures = all_pressures[-min(latest_number, len(all_pressures)):]
         card_title = f'Your latest {latest_number} pressures'
         card_text = ''
         card_image = Image(
-            'https://pressure-monitor-logo.s3-eu-west-1.amazonaws.com/pressure_monitor_medium.png',
-            'https://pressure-monitor-logo.s3-eu-west-1.amazonaws.com/pressure_monitor_large.png'
+            'https://drive.google.com/file/d/1NGt5K8C5gH92Xlfm6d_paCWfotAP-fRb/view?usp=sharing',
+            'https://drive.google.com/file/d/1wDiddNjJMhjkWhcKChH-Tt5BO06fmayU/view?usp=sharing'
         )
         speak_output = 'Your report was sent to your account. You can check it on the portal or the mobile app.'
 
-        self._log(f'all pressures: {all_pressures}')
         self._log(f'latest pressures: {latest_pressures}')
 
         for pressure in latest_pressures:
