2b7a0b3a3ff5bb1deaed383273a889ee339719ed
LOYANNE <loyanne.cristine@gmail.com>
2020-08-13 01:02:24 +0100

    Disable remove intent for the moment

--- lambda/handlers/intents/add_reminder.py
+++ lambda/handlers/intents/add_reminder.py
@@ -11,14 +11,14 @@ class AddReminderIntentHandler(BaseIntentHandler):
         super().__init__('AddReminderIntent')
 
     def handle(self, handler_input):
-        if self.reminder_at_time() is not None:
-            speak_output = 'Okay, this reminder is already set.'
-            return (
-                handler_input.response_builder
-                    .speak(speak_output)
-                    .ask(speak_output)
-                    .response
-            )
+        # if self.reminder_at_time() is not None:
+        #     speak_output = 'Okay, this reminder is already set.'
+        #     return (
+        #         handler_input.response_builder
+        #             .speak(speak_output)
+        #             .ask(speak_output)
+        #             .response
+        #     )
 
         system = self.handler_input.request_envelope.context.system
         hour = self.hour()
@@ -61,7 +61,7 @@ class AddReminderIntentHandler(BaseIntentHandler):
 
         if 'alertToken' in response_json:
             speak_output = 'Alright, I will remind you when the time comes.'
-            self.add_reminder(Reminder(identifier=response_json['alertToken'], hour=hour, minute=minute))
+            # self.add_reminder(Reminder(identifier=response_json['alertToken'], hour=hour, minute=minute))
         else:
             speak_output = 'Something went wrong, please try again.'
 
